ddb60f63db9b92ef484b56213f943269ecc1db73
soychrisjuncal <juncalcf@gmail.com>
2025-09-24 00:57:12 -0300

    fix: Add conditional check for GOOGLE_PRIVATE_KEY

--- server.js
+++ server.js
@@ -16,7 +16,7 @@ const client = twilio(process.env.TWILIO_ACCOUNT_SID, process.env.TWILIO_AUTH_TO
 const auth = new google.auth.GoogleAuth({
   credentials: {
     client_email: process.env.GOOGLE_SERVICE_ACCOUNT_EMAIL,
-    private_key: process.env.GOOGLE_PRIVATE_KEY.replace(/\\n/g, '\n'),
+private_key: process.env.GOOGLE_PRIVATE_KEY ? process.env.GOOGLE_PRIVATE_KEY.replace(/\\n/g, '\n') : '',
   },
   scopes: ['https://www.googleapis.com/auth/spreadsheets'],
 });
